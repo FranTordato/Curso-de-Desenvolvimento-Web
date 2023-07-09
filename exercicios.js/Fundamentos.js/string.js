b05d6f1796f78e4e77fbae6f82d12acae3995685
Fran Tordato <106915881+FranTordato@users.noreply.github.com>
2023-07-09 19:06:39 -0300

    Add files via upload

---
+++ exercicios.js/Fundamentos.js/string.js
@@ -0,0 +1,11 @@
+const nome='Francislaine';
+
+console.log(nome.charAt(4));//pega o 4 catacter
+console.log(nome.charCodeAt(3));//pega o indice da tabela unicode
+console.log(nome.indexOf('r'));//pega o indice da string 
+console.log(nome.substring(1));//lê a partir do indice(1)
+console.log(nome.substring(0,3));//le a partir do indice(0) até o (3)
+console.log(nome.replace('r','e'));//converte a letra (r)para(e)
+console.log(('Ana;Maria;Pedro').split(';'));//altera o simbolo separador para (,)e separa as string
+
+
